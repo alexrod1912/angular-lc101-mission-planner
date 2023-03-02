8389a9564ffd9f2c784146c52ae0487091fa2ec4
rodrin19 <65668704+rodrin19@users.noreply.github.com>
2023-03-02 12:07:46 -0600

    Need to disable buttons

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+   equipmentItems: object[] = [
+       {name: 'Duct Tape', mass: 0.5},
+       {name: 'Space Camera', mass: 20},
+       {name: 'Food', mass: 150},
+       {name: 'Oxygen Tanks', mass: 400},
+       {name: 'AE-35 Unit', mass: 5},
+       {name: 'ISS Supplies', mass: 800},
+       {name: 'Water', mass: 250},
+       {name: 'Satellite', mass: 1200},
+       {name: 'R2 Unit', mass: 32}
+   ];
+   cargoHold: object[] = [];
+   cargoMass: number = 0;
+   maximumAllowedMass: number = 2000;
+   maxItems: number = 10;
+
+
+
+addItem(equiptment: object) : boolean{
+  this.cargoHold.push(equiptment)
+  this.cargoMass = this.cargoMass + equiptment.mass
+  return this.maximumAllowedMass - this.cargoMass <= 200;
+  }
+
+
+
   constructor() { }
 
   ngOnInit() {
